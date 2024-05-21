6f2b5975b5187babc1cb149b9df19607087ed1d2
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-05-21 16:19:21 +0300

    Use pos.sql file in class path

--- src/mobiclick/application/db/DBInit.java
+++ src/mobiclick/application/db/DBInit.java
@@ -1,6 +1,8 @@
 package mobiclick.application.db;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -17,18 +19,19 @@ import static mobiclick.application.Application.LOGGER;
  * @author DennisMutethia
  */
 public class DBInit {
-    final static String filePath = "pos.sql";
+    private final static String filePath = "/mobiclick/application/db/pos.sql";
 
-    public static void loadSqlFile() {
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+    public void loadSqlFile() {
+        try (InputStream inputStream = getClass().getResourceAsStream(filePath);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             String sql = reader.lines().collect(Collectors.joining("\n"));
-            executeSql(sql);
+            this.executeSql(sql);
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "{0}", e);
         }
     }
 
-    private static void executeSql(String sql) {
+    private void executeSql(String sql) {
         try (Connection conn = DriverManager.getConnection(DB_URL)) {
             if (conn != null) {
                 System.out.println("Connected to the database.");
