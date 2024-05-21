154a68b3750374e37e7efa856544230ceaa0db7d
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-05-21 09:50:21 +0300

    change name

--- src/mobiclick/menu/Menu.java
+++ src/mobiclick/menu/Menu.java
@@ -65,7 +65,7 @@ public class Menu extends JPanel {
 
     private final List<MenuEvent> events = new ArrayList<>();
     private boolean menuFull = true;
-    private final String headerName = "mobiclick Channel";
+    private final String headerName = "Matrix POS";
 
     protected final boolean hideMenuTitleOnMinimum = true;
     protected final int menuTitleLeftInset = 5;
